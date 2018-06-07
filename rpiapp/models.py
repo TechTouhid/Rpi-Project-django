537027de5c692e1abbb4167f90321ff1d392c9e3
touhid <touhidalamin123@gmail.com>
2018-06-07 19:27:53 +0600

    create student and subject model

--- rpiapp/models.py
+++ rpiapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    SHIFT = (
+        ('1', '1st'),
+        ('2', '2nd'),
+    )
+    SEMESTER = (
+        ('1', '1st'),
+        ('2', '2nd'),
+        ('3', '3rd'),
+        ('4', '4th'),
+        ('5', '5th'),
+        ('6', '6th'),
+        ('7', '7th'),
+        ('8', '8th'),
+    )
+
+    DEPARTMENT = (
+        ('computer', 'Computer'),
+        ('civil', 'Civil'),
+        ('electrical', 'Electrical'),
+        ('electronics', 'Electronics'),
+        ('mechanical', 'Mechanical'),
+        ('power', 'Power'),
+        ('electromedical', 'Electromedical'),
+        ('mechatronics', 'Mechatronics'),
+    )
+
+    s_name = models.CharField(max_length=250, blank=True)
+    s_roll = models.IntegerField(default=0, blank=True)
+    s_reg = models.IntegerField(default=0, blank=True)
+    s_sift = models.CharField(max_length=10, choices=SHIFT)
+    s_semester = models.CharField(max_length=10, choices=SEMESTER)
+    s_session = models.CharField(max_length=10, choices=SEMESTER)
+    s_department = models.CharField(max_length=50, choices=DEPARTMENT)
+
+class Subject(models.Model):
+    sub_name = models.CharField(max_length=120, default='')
+    sub_code = models.IntegerField(default=0)
+    sub_credit = models.IntegerField(default=0)
+    full_mark = models.IntegerField(default=0)
+    tc = models.IntegerField(default=0)
+    tf = models.IntegerField(default=0)
+    pc = models.IntegerField(default=0)
+    pf = models.IntegerField(default=0)
